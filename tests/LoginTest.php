9441aa8b4aff4cc3f815d3a05ba3adb1ef617e9a
StevenDao <nataku235@gmail.com>
2016-03-22 01:32:19 -0400

    Implemented Login Test Cases using Screen interactions

---
+++ tests/LoginTest.php
@@ -0,0 +1,94 @@
+<?php
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class LoginTest extends TestCase
+{
+	public function setUp()
+	{
+		// This method will automatically be called prior to any of your test cases
+		parent::setUp();
+	}
+
+	/**
+	 * A basic test example.
+	 *
+	 * @return void
+	 */
+	public function testValidLogin()
+	{
+		// Login Page
+		$this->visit('/login')
+		     ->see('SimpleStore')
+		     ->see('Login')
+		     ->see('E-Mail')
+		     ->type('admin@test.com', 'email')
+		     ->see('Password')
+		     ->type('admin123', 'password')
+		     ->press('Login')
+		     ->seePageIs('/')
+		     ->see('Product List');
+	}
+
+	public function testBlankLogin()
+	{
+		// Login Page
+		$this->visit('/login')
+		     ->see('SimpleStore')
+		     ->see('Login')
+		     ->see('E-Mail')
+		     ->see('Password')
+		     ->press('Login')
+		     ->seePageIs('/login')
+		     ->see('The email field is required.')
+		     ->see('The password field is required.');
+	}
+
+	public function testForgotPassword()
+	{
+		// Login Page
+		$this->visit('/login')
+		     ->see('SimpleStore')
+		     ->see('Login')
+		     ->see('E-Mail')
+		     ->see('Password')
+		     ->click('Forgot Your Password?');
+		// Reset Page
+		$this->seePageIs('/password/reset')
+		     ->see('Reset Password')
+		     ->see('E-Mail Address')
+		     ->type('admin@test.com', 'email')
+		     ->press('Send Password Reset Link');
+		// Page Reload with Success message
+		$this->see('We have e-mailed your password reset link!');
+		// Try again with blank email
+		$this->press('Send Password Reset Link')
+		     ->see('The email field is required.');
+	}
+
+	public function testRegistration()
+	{
+		// Login Page
+		$this->visit('/login')
+		     ->see('SimpleStore')
+		     ->see('Login')
+		     ->see('E-Mail')
+		     ->see('Password')
+		     ->click('Not A Member?');
+		// Registration Page
+		$password = $this->faker->password;
+		$this->seePageIs('/register')
+		     ->see('Name')
+		     ->type($this->faker->name, 'name')
+		     ->see('E-Mail Address')
+		     ->type($this->faker->email, 'email')
+		     ->see('Password')
+		     ->type($password, 'password')
+		     ->see('Confirm Password')
+		     ->type($password, 'password_confirmation')
+		     ->press('Register');
+		// Logs in after Registration
+		$this->seePageIs('/');
+	}
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,24 +2,30 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://localhost';
+	/**
+	 * The base URL to use while testing the application.
+	 *
+	 * @var string
+	 */
+	protected $baseUrl = 'http://localhost';
+	protected $faker;
 
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
+	public function setUp()
+	{
+		parent::setUp();
+		$this->faker = Faker\Factory::create();
+	}
 
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+	/**
+	 * Creates the application.
+	 *
+	 * @return \Illuminate\Foundation\Application
+	 */
+	public function createApplication()
+	{
+		$app = require __DIR__ . '/../bootstrap/app.php';
+		$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
-        return $app;
-    }
+		return $app;
+	}
 }
